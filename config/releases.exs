8f201c427415933b2e7099c0cea4af52ba3d9442
Leonardo Ugiete <leonardougiete@hotmail.com>
2021-06-02 00:16:55 -0300

    update versions

--- config/releases.exs
+++ config/releases.exs
@@ -27,12 +27,4 @@ config :team_budget, TeamBudgetWeb.Endpoint,
   url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
   secret_key_base: secret_key_base
 
-# ## Using releases (Elixir v1.9+)
-#
-# If you are doing OTP releases, you need to instruct Phoenix
-# to start each relevant endpoint:
-#
 config :team_budget, TeamBudgetWeb.Endpoint, server: true
-#
-# Then you can assemble a release by calling `mix release`.
-# See `mix help release` for more information.
