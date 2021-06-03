91ea8c317492771ae2ec49b320aeb66bb39ae876
Leonardo Ugiete <leonardougiete@hotmail.com>
2021-06-03 00:18:47 -0300

    seed example

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,11 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     TeamBudget.Repo.insert!(%TeamBudget.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias TeamBudget.{Accounts.User, Repo}
+
+%{
+  first_name: "Name",
+  last_name: "Surname",
+  email: "example@email.com",
+  password_hash: "pswd@123"
+}
+|> User.changeset()
+|> Repo.insert()
+|> IO.inspect()
