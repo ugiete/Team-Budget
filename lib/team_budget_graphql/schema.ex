af30d73a5823b69ea6529abb4b3fa847ea308d17
Leonardo Ugiete <leonardougiete@hotmail.com>
2021-06-05 21:13:58 -0300

    accept any user to call list accounts

--- lib/team_budget_graphql/schema.ex
+++ lib/team_budget_graphql/schema.ex
@@ -17,7 +17,7 @@ defmodule TeamBudgetGraphql.Schema do
   query do
     @desc "Get list of all users"
     field :list_users, list_of(:user) do
-      middleware(Middlewares.Authorize, :admin)
+      middleware(Middlewares.Authorize, :user)
       resolve(&Resolvers.UserResolver.list_users/3)
     end
   end
